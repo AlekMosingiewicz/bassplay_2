d6a22501b832e1dabd7bb7c2cf664798086aac27
Aleksander Mosingiewicz <aleksander@simplified.co>
2022-07-04 15:26:56 +0200

    Song info frame.

---
+++ ui/SongInfoFrame.cpp
@@ -0,0 +1,8 @@
+//
+// Created by aleksander on 04.07.22.
+//
+
+#include "SongInfoFrame.h"
+
+namespace Bassplay {
+} // Bassplay
---
+++ ui/SongInfoFrame.h
@@ -0,0 +1,19 @@
+//
+// Created by aleksander on 04.07.22.
+//
+
+#ifndef BASSPLAY_2_SONGINFOFRAME_H
+#define BASSPLAY_2_SONGINFOFRAME_H
+
+#include "../play/Song.h"
+
+namespace Bassplay::Ui {
+
+    class SongInfoFrame {
+    private:
+        Play::Song* m_song = nullptr;
+    };
+
+} // Bassplay
+
+#endif //BASSPLAY_2_SONGINFOFRAME_H
