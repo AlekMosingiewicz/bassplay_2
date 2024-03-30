9a4f4a913812a77bed8f714e6548346cdcff125a
Alek Mosingiewicz <almos666@gmail.com>
2024-03-30 10:54:11 +0100

    JSONNode structure.

--- play/parser/JsonNode.h
+++ play/parser/JsonNode.h
@@ -13,8 +13,9 @@ struct JsonNode {
     std::string m_name;
     std::map<std::string, JsonNode> children;
     void *m_value;
+    JsonNode *m_parent = nullptr;
 
-    JsonNode(std::string name, void *value): m_name (std::move(name)), m_value(value) {}
+    JsonNode(std::string name, void *value, JsonNode *parent = nullptr): m_name (std::move(name)), m_value(value) {}
 };
 
 #endif //BASSPLAY_2_JSONNODE_H
